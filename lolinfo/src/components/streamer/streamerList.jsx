ef67457ec2c98865385c517a7af0a063ba781300
jaeyeah <limjh819@naver.com>
2025-12-31 00:38:01 +0900

    스트리머 목록 출력 및 스타일링 추가

--- lolinfo/src/components/streamer/streamerList.jsx
+++ lolinfo/src/components/streamer/streamerList.jsx
@@ -1,16 +1,17 @@
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
-
+import { Link } from "react-router-dom"
+import "./streamer.css";
 
 export default function StreamerList() {
 
-    const [streamerList, setStreamerList] = useState({});
+    const [streamerList, setStreamerList] = useState([]);
 
     const loadData = useCallback( async() => {
         try {
-            const response = await axios.get("/streamer/");
-            console.log(response);
-            //setStreamerList(response.data);
+            const {data} = await axios.get("/streamer/");
+            setStreamerList(data);
+            console.log(data);
         } catch (error) {
             console.error("Error fetching streamer list:", error);
         }
@@ -23,7 +24,26 @@ export default function StreamerList() {
     //render
     return (<>
     
-    임시페이지
+    <div className="row">
+        <div className="col-12">
+            {streamerList.map((streamer)=>(
+                <div key={streamer.streamerNo} className="card mb-3">
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={streamer.streamerProfile} className="streamer-profile img-fluid rounded-start" alt={streamer.name} />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <span className="card-title">{streamer.streamerName}</span>
+                                <span className="card-text">{streamer.streamerSoopId}</span>
+                                <Link to={streamer.streamerStation} className="btn btn-primary ms-3" target="_blank" rel="noopener noreferrer">방송국</Link> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    </div>
     
     
     </>)
