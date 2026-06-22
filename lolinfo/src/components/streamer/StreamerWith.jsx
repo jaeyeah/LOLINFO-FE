05c391846d62d16f72fe5c6e5914a37b7b37890e
jaeyeah <limjh819@naver.com>
2026-06-22 19:14:21 +0900

    같은 대회팀 스트리머 목록

--- lolinfo/src/components/streamer/StreamerWith.jsx
+++ lolinfo/src/components/streamer/StreamerWith.jsx
@@ -11,10 +11,11 @@ export default function StreamerWith(){
 
     //로딩중 설정
     const [withCk, setWithCk] = useState([]);
+    const [withTournament, setWithTournament] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const loadData = useCallback(async()=>{
+    const loadCkData = useCallback(async()=>{
         try {
             setLoading(true);
             setError(null);
@@ -29,11 +30,32 @@ export default function StreamerWith(){
             }
             finally {
             setLoading(false);
+        }
+    }, [streamerId]);
+
+    const loadTournamentData = useCallback(async()=>{
+        try {
+            setLoading(true);
+            setError(null);
+            const { data } = await axios.get("/streamer/withTournament", {
+                params: { streamerId }
+            });
+            setWithTournament(data);
+            console.log(data)
+            } catch (error) {
+            console.error("Error fetching streamer detail:", error);
+            setError("스트리머 정보를 불러오지 못했습니다.");
             }
-        }, [streamerId]);
+            finally {
+            setLoading(false);
+        }
+    }, [streamerId]);
+
+
 
     useEffect(()=>{
-        loadData();
+        loadCkData();
+        loadTournamentData();
     },[]);
 
     const getWinRateColor = (rate) => {
@@ -47,10 +69,10 @@ export default function StreamerWith(){
 return (<>
 <div className="mt-3 row">
     {/* 맞라인 상대별 전적 */}
-    <div className="col-12 col-xl-4">
+    <div className="col-12 col-xl-6">
         <div className="card bg-dark border-secondary h-100">
         <div className="card-header bg-white border-secondary d-flex justify-content-between align-items-center">
-            <h5 className="mb-0 fw-bold section-title">같은팀 스트리머 전적</h5>
+            <h5 className="mb-0 fw-bold section-title">같은 CK팀 스트리머 전적</h5>
         </div>
         <div className="card-body">
             {loading && (
@@ -101,8 +123,49 @@ return (<>
     </div>
 
     {/* ㅁㄴㅇㄹ */}
-    <div className="col-12 col-xl-8">
-        
+    <div className="col-12 col-xl-6">
+        <div className="card bg-dark border-secondary h-100">
+        <div className="card-header bg-white border-secondary d-flex justify-content-between align-items-center">
+            <h5 className="mb-0 fw-bold section-title">같은 대회팀 스트리머 전적</h5>
+        </div>
+        <div className="card-body">
+            {loading && (
+            <div className="d-flex justify-content-center py-4">
+                <div className="spinner-border text-light" role="status" />
+            </div>
+            )}
+
+            {error && (
+            <div className="alert alert-danger" role="alert">
+                {vsError}
+            </div>
+            )}
+
+            {!loading && !error && (
+            <div className="list-group list-group-flush">
+                {withTournament.map((withTournament) => (
+                <div key={withTournament.partnerNo} className="list-group-item bg-dark border-secondary text-white py-3 vs-item">
+                    <div className="d-flex justify-content-between align-items-center gap-3">
+                    <div className="min-w-0">
+                        <div className="fw-semibold text-truncate vs-item-name">
+                        <Link to={`/streamer/${withTournament.partnerNo}`} className="fs-6 text-decoration-none text-white">
+                            <img src={buildProfileUrl(withTournament.partnerSoopId)}
+                                className="ck-participant-avatar" alt={withTournament.partnerName || ""}/>
+                            <span className="ms-2">{withTournament.partnerName}</span>
+                        </Link>
+                        <span className="ms-3 fw-semibold mb-2 vs-item-rate me-3 fs-5">{withTournament.playCount}회</span>
+                        </div>
+                    </div>
+                    <div className="text-end">
+                    </div>
+                        <button className="btn btn-positive">상세보기</button>
+                    </div>
+                </div>
+                ))}
+            </div>
+            )}
+        </div>
+        </div>
     </div>
 
 </div>
