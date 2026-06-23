d6060453bb3b5e788c5d90a831d8807b774bfe29
jaeyeah <limjh819@naver.com>
2026-06-23 13:43:23 +0900

    이미지 레이지 로딩 적용

--- lolinfo/src/components/streamer/StreamerWith.jsx
+++ lolinfo/src/components/streamer/StreamerWith.jsx
@@ -129,8 +129,8 @@ return (<>
                     <div className="min-w-0">
                         <div className="fw-semibold text-truncate vs-item-name">
                         <Link to={`/streamer/${withCk.partnerNo}`} className="fs-6 text-decoration-none text-white">
-                            {/* <img src={buildProfileUrl(withCk.partnerSoopId)}
-                                className="ck-participant-avatar" alt={withCk.partnerName || ""}/> */}
+                            <img src={buildProfileUrl(withCk.partnerSoopId)} loading="lazy"
+                                className="ck-participant-avatar" alt={withCk.partnerName || ""}/>
                             <span className="ms-2 fs-5">{withCk.partnerName}</span>
                         </Link>
                             <span className="ms-3 text-secondary">with</span>
@@ -185,7 +185,7 @@ return (<>
                         <div className="col-sm col-12">
                             <div className="fw-semibold text-truncate vs-item-name">
                             <Link to={`/streamer/${withTournament.partnerNo}`} className="fs-6 text-decoration-none text-white">
-                                <img src={buildProfileUrl(withTournament.partnerSoopId)}
+                                <img src={buildProfileUrl(withTournament.partnerSoopId)} loading="lazy"
                                     className="ck-participant-avatar" alt={withTournament.partnerName || ""}/>
                                 <span className="ms-2 me-2 fs-5">{withTournament.partnerName}</span>
                             </Link>
