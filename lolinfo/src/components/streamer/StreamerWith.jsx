0a6e08ada7aa57e0b8ac02fff238e44e212555fd
jaeyeah <limjh819@naver.com>
2026-06-23 12:10:47 +0900

    팀메이트 - 승률별 색상조정, 반응형 웹디자인 적용

--- lolinfo/src/components/streamer/StreamerWith.jsx
+++ lolinfo/src/components/streamer/StreamerWith.jsx
@@ -24,7 +24,7 @@ export default function StreamerWith(){
                 params: { streamerId }
             });
             setWithCk(data);
-            console.log("CK 데이터 : ",data)
+            //console.log("CK 데이터 : ",data)
             } catch (error) {
             console.error("Error fetching streamer detail:", error);
             setCkError("스트리머 CK정보를 불러오지 못했습니다.");
@@ -42,7 +42,7 @@ export default function StreamerWith(){
                 params: { streamerId }
             });
             setWithTournament(data);
-            console.log("CK 데이터 : ",data)
+            //console.log("CK 데이터 : ",data)
         } catch (error) {
             console.error("Error fetching streamer detail:", error);
             setTournamentError("스트리머 대회정보를 불러오지 못했습니다.");
@@ -60,9 +60,15 @@ export default function StreamerWith(){
     },[]);
 
     const getWinRateColor = (rate) => {
-        if (rate >= 70) return "#2ecc71";
-        if (rate >= 55) return "#4dabf7";
-        if (rate >= 45) return "#f6c23e";
+        if (rate >= 100) return "#39dd46";
+        if (rate >= 70) return "#38cc76";
+        if (rate >= 60) return "#1b95fa";
+        if (rate >= 55) return "#abd9ff";
+        if (rate >= 50) return "#bdbcbc";
+        if (rate >= 45) return "#b3aa91";
+        if (rate >= 40) return "#f8b200";
+        if (rate >= 35) return "#e7893c";
+        if (rate >= 30) return "#e75e3c";
         return "#e74c3c";
     };
 
@@ -70,10 +76,10 @@ export default function StreamerWith(){
 return (<>
 <div className="mt-3 row">
     {/* 같이 CK 참여했던 스트리머 목록 */}
-    <div className="col-12 col-xl-6">
+    <div className="col-12 col-xl-6 mb-3">
         <div className="card bg-dark border-secondary h-100">
         <div className="card-header bg-white border-secondary d-flex justify-content-between align-items-center">
-            <h5 className="mb-0 fw-bold section-title">같은 CK팀 스트리머 전적</h5>
+            <h5 className="mb-0 fw-bold section-title">CK 팀메이트 승률</h5>
         </div>
         <div className="card-body">
             {loading && (
@@ -100,8 +106,9 @@ return (<>
                                 className="ck-participant-avatar" alt={withCk.partnerName || ""}/>
                             <span className="ms-2">{withCk.partnerName}</span>
                         </Link>
-                            <span className="ms-3 text-secondary vs-item-record mt-1">
-                            : {withCk.playCount}전 {withCk.winCount}승 {withCk.loseCount}패
+                            <span className="ms-3 text-secondary">with</span>
+                            <span className="ms-2  vs-item-record mt-1">
+                                {withCk.playCount}전 {withCk.winCount}승 {withCk.loseCount}패
                             </span>
                         </div>
                     </div>
@@ -127,7 +134,7 @@ return (<>
     <div className="col-12 col-xl-6">
         <div className="card bg-dark border-secondary h-100">
         <div className="card-header bg-white border-secondary d-flex justify-content-between align-items-center">
-            <h5 className="mb-0 fw-bold section-title">같은 대회팀 스트리머 전적</h5>
+            <h5 className="mb-0 fw-bold section-title">대회 팀메이트</h5>
         </div>
         <div className="card-body">
             {loading && (
@@ -146,8 +153,9 @@ return (<>
             <div className="list-group list-group-flush">
                 {withTournament.map((withTournament) => (
                 <div key={withTournament.partnerNo} className="list-group-item bg-dark border-secondary text-white py-3 vs-item">
-                    <div className="d-flex justify-content-between align-items-center gap-3">
-                        <div className="min-w-0">
+                    {/* <div className="d-flex justify-content-between align-items-center gap-3"> */}
+                    <div className="row align-items-center g-2">
+                        <div className="col-sm col-12">
                             <div className="fw-semibold text-truncate vs-item-name">
                             <Link to={`/streamer/${withTournament.partnerNo}`} className="fs-6 text-decoration-none text-white">
                                 <img src={buildProfileUrl(withTournament.partnerSoopId)}
@@ -167,10 +175,19 @@ return (<>
                                 )}
                             </div>
                         </div>
-                        
-                        <div className="text-end">
-                            <button className="btn btn-positive bg-secondary text-white" disabled>상세보기</button>
+                        {/* 상세버튼 - PC용 */}
+                        <div className="col-sm-auto d-none d-sm-block text-end">
+                            <button className="btn btn-sm btn-outline-secondary"
+                                //onClick={...}
+                            > 상세보기 </button>
                         </div>
+                        {/* 상세버튼 - 모바일용 */}
+                        <div className="col-12 d-sm-none text-end">
+                            <button className="btn btn-outline-secondary w-100"
+                                //onClick={...}
+                            > 상세보기 </button>
+                        </div>
+                        
                     </div>
                 </div>
                 ))}
