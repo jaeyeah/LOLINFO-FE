e5d3744f2ee37333c28e7543463268c3b6691214
jaeyeah <limjh819@naver.com>
2026-09-03 13:09:01 +0900

    FOOTER 하단배치 CSS 적용

--- lolinfo/src/App.jsx
+++ lolinfo/src/App.jsx
@@ -18,11 +18,17 @@ function App() {
     <>
       {/* Router는 주소에 의한 화면 분할을 처리하는 도구이며 설정된 영역 내에서만 작동함 */}
       <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
-        <Menu/>
-          <div className="container-fluid my-5 pt-5">
-            <Content/>
-          </div>
-        <Footer />
+        <div className="app-layout">
+          <Menu/>
+
+          <main className="app-content">
+            <div className="container-fluid my-5 pt-5">
+              <Content/>
+            </div>
+          </main>
+          
+          <Footer />
+        </div>
       </BrowserRouter>
     </>
   )
