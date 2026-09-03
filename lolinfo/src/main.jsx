38ad8c1ffd190b1273692a919a5c5ae782ea5234
jaeyeah <limjh819@naver.com>
2026-09-03 16:52:02 +0900

    헬멧프로바이더 추가

--- lolinfo/src/main.jsx
+++ lolinfo/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HelmetProvider } from 'react-helmet-async'
 
 //axios
 import "./utils/axios/index.js"
