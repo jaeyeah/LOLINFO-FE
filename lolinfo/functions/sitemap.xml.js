21b0fe197d50af41894b54bc942f8e1cd8e250e2
jaeyeah <limjh819@naver.com>
2026-09-04 20:51:08 +0900

    사이트맵 js 수정 (캐시추가)

--- lolinfo/functions/sitemap.xml.js
+++ lolinfo/functions/sitemap.xml.js
@@ -1,14 +1,42 @@
-export async function onRequest() {
+export async function onRequest(context) {
+
+    const cache = caches.default;
+    const cacheKey = new Request(context.request.url, {
+        method: "GET"
+    });
+
+    // Cloudflare 캐시 확인
+    let cachedResponse = await cache.match(cacheKey);
+
+    if (cachedResponse) {
+        return cachedResponse;
+    }
+
+    // 캐시에 없으면 Render 호출
     const response = await fetch(
         "https://lolinfo.onrender.com/sitemap.xml"
     );
 
+    if (!response.ok) {
+        return new Response("Failed to load sitemap", {
+            status: response.status
+        });
+    }
+
     const xml = await response.text();
 
-    return new Response(xml, {
-        status: response.status,
+    const sitemapResponse = new Response(xml, {
+        status: 200,
         headers: {
             "Content-Type": "application/xml; charset=UTF-8",
-        },
+            "Cache-Control": "public, max-age=86400"
+        }
     });
+
+    // 하루 캐시
+    context.waitUntil(
+        cache.put(cacheKey, sitemapResponse.clone())
+    );
+
+    return sitemapResponse;
 }
