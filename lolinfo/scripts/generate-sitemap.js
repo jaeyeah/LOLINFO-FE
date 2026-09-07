05bd7dd278973b50f60ca12a603d974ba99df741
jaeyeah <limjh819@naver.com>
2026-09-07 09:04:05 +0900

    사이트맵 수정

--- lolinfo/scripts/generate-sitemap.js
+++ lolinfo/scripts/generate-sitemap.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-const API_URL = "https://lolinfo.onrender.com";
+const API_URL = "https://sooplol.onrender.com";
 
 const response = await fetch(`${API_URL}/api/sitemap/data`);
 
@@ -17,19 +17,33 @@ const urls = [
     "https://sooplol.com/tournament",
 ];
 
+// 스트리머 기본 상세: 등록된 전체 스트리머
 for (const streamerNo of data.streamers) {
-    const baseUrl = `https://sooplol.com/streamer/${streamerNo}`;
+    urls.push(`https://sooplol.com/streamer/${streamerNo}`);
+}
 
-    urls.push(baseUrl);
-    urls.push(`${baseUrl}/tournaments`);
-    urls.push(`${baseUrl}/ck-records`);
-    urls.push(`${baseUrl}/streamerWith`);
+// 대회 참가 기록이 있을 때만
+for (const streamerNo of data.tournamentStreamers) {
+    urls.push(`https://sooplol.com/streamer/${streamerNo}/tournaments`);
+}
+
+// CK 기록이 있을 때만
+for (const streamerNo of data.ckStreamers) {
+    urls.push(`https://sooplol.com/streamer/${streamerNo}/ck-records`);
+}
+
+// 대회 또는 CK 기록이 있으면 동료 전적 화면도 생성
+const withStreamerNos = new Set([
+    ...data.tournamentStreamers,
+    ...data.ckStreamers,
+]);
+
+for (const streamerNo of withStreamerNos) {
+    urls.push(`https://sooplol.com/streamer/${streamerNo}/streamerWith`);
 }
 
 for (const tournamentId of data.tournaments) {
-    urls.push(
-        `https://sooplol.com/tournament/${tournamentId}`
-    );
+    urls.push(`https://sooplol.com/tournament/${tournamentId}`);
 }
 
 const xml = `<?xml version="1.0" encoding="UTF-8"?>
